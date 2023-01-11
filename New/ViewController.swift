581544c4845d4e70e700220ec0076317e449ca41
Demetris <snakeye-cy@hotmail.com>
2023-01-11 19:39:57 +0200

    added new Controller

--- New/ViewController.swift
+++ New/ViewController.swift
@@ -2,18 +2,16 @@
 //  ViewController.swift
 //  New
 //
-//  Created by Demetris Georgiou on 12/04/2022.
+//  Created by Demetris Georgiou on 11/01/2023.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController:UIViewController{
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let new = "jhksdhdkfsjdl"
-        // Do any additional setup after loading the view.
+        let text = "jhfkdsjhf"
     }
-
 }
-
